f8dd277ff074fdd6cf16912e1a66f1c649fa88c5
Matthieu Vanhoutte <matthieuvanhoutte@gmail.com>
2024-01-26 19:46:51 +0100

    Commit the frontend for Vercel deployed weatherapp

---
+++ login.js
@@ -0,0 +1,44 @@
+const urlBackend = "http://localhost:3000";
+
+document.querySelector("#register").addEventListener("click", function () {
+  fetch(`${urlBackend}/users/signup`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      name: document.querySelector("#registerName").value,
+      email: document.querySelector("#registerEmail").value,
+      password: document.querySelector("#registerPassword").value,
+    }),
+  })
+    .then((response) => response.json())
+    .then((apiData) => {
+      if (apiData.result) {
+        console.log(true);
+        // Navigate to the Location.reload article
+        window.location.assign("./index.html");
+      } else {
+        console.log(apiData.error);
+      }
+    });
+});
+
+document.querySelector("#connection").addEventListener("click", function () {
+  fetch(`${urlBackend}/users/signin`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: document.querySelector("#connectionEmail").value,
+      password: document.querySelector("#connectionPassword").value,
+    }),
+  })
+    .then((response) => response.json())
+    .then((apiData) => {
+      if (apiData.result) {
+        console.log(true);
+        // Navigate to the Location.reload article
+        window.location.assign("./index.html");
+      } else {
+        console.log(apiData.error);
+      }
+    });
+});
